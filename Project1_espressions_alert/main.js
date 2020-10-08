0dcf191c51ab936bf86b6ac787a8332197b1ea35
Reece <ReeceHarris423@gmail.com>
2020-10-08 04:49:24 -0600

    Updating index and project 1 JS

--- Project1_espressions_alert/main.js
+++ Project1_espressions_alert/main.js
@@ -1,9 +1,35 @@
-document.write("Hello, World!");
 
-var Z = "All Hail Planketon."; 
 
-window.alert(Z)
+document.write (4 * 3); //Here is a function. 
 
-var Y = " I'm not very creative."; 
+document.write("Hello, World!"); //Basic Print out of text
 
-document.write(Y)
+var Z = "All Hail Planketon."; //Assigned variable Z (Plan Z reference)
+
+window.alert(Z)  //Page alert via pop up displaying value assigned to var Z
+
+var Y = " I'm not very creative."; //Assigned Var Y
+
+document.write(Y) //Print Var Y
+
+
+var str1 ="Line 1.";  // Assigned var
+var str2 = "Line 2";
+
+var res = str1.concat(str2); //concatenated str1 and str 2 
+
+document.write(res);
+
+document.write ("This is a sentance. This isn\'t creative. I\m not good on the spot.")
+//above is showing slashes to allow apostraphies to show up in the text. 
+
+
+var Ammo = "Caliber", AK47 = "7.62", M4 = "5.56", Barret = ".50", Glock = ".40";
+
+document.write(M4);  // displays value from string ammo in the caliber family
+document.write(Glock);
+
+
+var Barbie = "I\'m a barbie girl.";  // declaration of Barbie value 
+var Barbie = Barbie.fontcolor("pink"); // modifies color of text that Barbie displays to pink
+document.write(Barbie);// My maturity waynes around 3 am. 
