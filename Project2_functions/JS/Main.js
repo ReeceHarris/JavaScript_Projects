25b9dc584f6fc1be475ee9e6cbc27b9b04b263a7
Reece <ReeceHarris423@gmail.com>
2020-10-08 20:13:15 -0600

    Added Functions assignment

---
+++ Project2_functions/JS/Main.js
@@ -0,0 +1,14 @@
+var A = "Thank you for pressing the button.";
+var A = A.fontcolor("red"); //Declares var A text string and changes font color of text to red
+
+function myFunction () {  
+   var str1 = "Value from str1";
+   str1 += "Added text to str1.";
+   document.getElementById("Concatenate").innerHTML = str1;
+} //myFunction produces the text from var str1 with concatenated info added
+
+
+
+
+
+
