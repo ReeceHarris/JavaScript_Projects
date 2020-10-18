203e6cbbf48545ef79d8aa404f68ed135154e177
Reece <ReeceHarris423@gmail.com>
2020-10-18 00:29:46 -0600

    Comments

--- Project9_countdown_slideshow/JS/Main.js
+++ Project9_countdown_slideshow/JS/Main.js
@@ -13,16 +13,16 @@
    showSlides(slideIndex = n);
  }
 
- function showSlides(n) {
-   var i;
-   var slides = document.getElementsByClassName("mySlides");
+ function showSlides(n) { //Function for showing slides
+   var i; //declaring variable
+   var slides = document.getElementsByClassName("mySlides"); 
    var dots = document.getElementsByClassName("dot");
-   if (n > slides.length) {slideIndex = 1}    
-   if (n < 1) {slideIndex = slides.length}
-   for (i = 0; i < slides.length; i++) {
+   if (n > slides.length) {slideIndex = 1}    //if statement to determine which slide to use
+   if (n < 1) {slideIndex = slides.length}   
+   for (i = 0; i < slides.length; i++) {     //for statement to incremement slide index by 1 
        slides[i].style.display = "none";  
    }
-   for (i = 0; i < dots.length; i++) {
+   for (i = 0; i < dots.length; i++) {  //increments dots 1 to maintain accurate position
       dots[i].className = dots[i].className.replace(" active", "");
   }
    slides[slideIndex-1].style.display = "block";  
@@ -32,14 +32,14 @@
  
  
  
- function countdown() {
+ function countdown() {  //basic countdown function
    var seconds = document.getElementById("seconds").value;
 
-   function tick() {
+   function tick() {  //function for tick rate of the countdown
       seconds = seconds -1; 
       timer.innerHTML = seconds;
-      setTimeout(tick, 1000);
-      if(seconds == -1){
+      setTimeout(tick, 1000); //setting tick rate to 1: 1000ms 
+      if(seconds == -1){   //if statement for alert that timer is up
          alert("Time's up!");
       }  
          }
