b094c2faec292b2164dd8c6ad38e516ad130df78
Reece <ReeceHarris423@gmail.com>
2020-10-18 22:13:49 -0600

    Organizing files

--- Project10/JS/main.js
+++
@@ -1,8 +0,0 @@
-var A = myFunction(5,8);
-document.getElementById("Math").innerHTML = A; 
-var a = 5;
-var b = 8; 
-function myFunction(a, b) {
-   return a + b; 
-} 
-
---
+++ Project10_loops_arrays/JS/misc1.js
@@ -0,0 +1,6 @@
+function myFunction() { 
+   var subtraction = 12 - 9; 
+   document.getElementById("Math").innerHTML= "12-9 = " + subtraction;
+   
+}
+
